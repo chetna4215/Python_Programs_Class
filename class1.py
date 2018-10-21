b12ce5245495216fd24888ffa5318553051fc5e3
chetna4215 <chetna.mudgale@gmail.com>
2018-10-21 13:27:26 +0530

    This is initial commit

---
+++ class1.py
@@ -0,0 +1,9 @@
+"""
+This is class 1 file
+1) we set up python3.7,git, pyharm
+2) created github repository
+3) created virtual environment
+4) setup pycharm project
+
+"""
+print(" Welcome Python ")
